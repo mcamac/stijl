bd0a4d2c632992abd31f187097e4fcc20857f53d
Luis Carli <luiscarli@kensho.com>
2016-03-29 17:02:03 -0400

    defaultTheme

---
+++ src/defaultTheme.js
@@ -0,0 +1,14 @@
+
+const size = 16
+
+export const defaultTheme = {
+  size,
+  lineHeight: 1.5,
+  gutter: size * 0.5,
+  typeRatio: 1.2,
+  background: 'hsl(0, 0%, 100%)',
+  text: 'hsl(0, 0%, 0%)',
+  link: 'hsl(197, 72%, 33%)',
+  active: 'hsl(34, 67%, 49%)',
+  card: 'hsl(0, 0%, 89%)',
+}
