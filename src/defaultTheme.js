a70f4e753ec9ecc0c9b4ad7ab94b454432904df6
Luis Carli <luiscarli@kensho.com>
2016-03-30 14:19:45 -0400

    defaultTheme: update names and colors

--- src/defaultTheme.js
+++ src/defaultTheme.js
@@ -1,14 +1,17 @@
 
-const size = 16
+const fontSize = 16
 
 export const defaultTheme = {
-  size,
+  fontSize,
   lineHeight: 1.5,
-  gutter: size * 0.5,
-  typeRatio: 1.2,
+  gutter: fontSize * 0.5,
+  textBasis: 600,
+  fontFamily: 'Open Sans',
+
+  // colors
   background: 'hsl(0, 0%, 100%)',
   text: 'hsl(0, 0%, 0%)',
-  link: 'hsl(197, 72%, 33%)',
+  action: 'hsl(197, 72%, 30%)',
   active: 'hsl(34, 67%, 49%)',
-  card: 'hsl(0, 0%, 89%)',
+  card: 'hsl(0, 0%, 92%)',
 }
