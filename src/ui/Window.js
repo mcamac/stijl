97adad81b6d6b3e5008a87153d3b9c50ecfca821
Luis Carli <luiscarli@kensho.com>
2016-03-30 14:22:10 -0400

    Window: use font props from theme

--- src/ui/Window.js
+++ src/ui/Window.js
@@ -1,15 +1,23 @@
 
 import React from 'react'
 import * as ui from '../ui'
+import {getTheme} from '../getTheme'
 
-export const Window = (props) =>
-  <ui.Col
-    bottom={0}
-    left={0}
-    overflow='scroll'
-    position='absolute'
-    right={0}
-    top={0}
-    WebkitOverflowScrolling='touch'
-    {...props}
-  />
+export const Window = React.createClass({
+  mixins: [getTheme],
+  render() {
+    const theme = this.getTheme()
+    return <ui.Div
+      bottom={0}
+      left={0}
+      overflow='scroll'
+      position='absolute'
+      right={0}
+      top={0}
+      WebkitOverflowScrolling='touch'
+      fontFamily={theme.fontFamily}
+      fontSize={theme.fontSize}
+      {...this.props}
+    />
+  },
+})
