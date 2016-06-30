5f6b177a9cffc28ca0349e6a021974162a10d61f
Luis Carli <luiscarli@kensho.com>
2016-06-30 17:25:41 -0400

    (BRK) Window: Col with 100% height
    needs html/body/root with 100% height

--- src/ui/Window.js
+++ src/ui/Window.js
@@ -16,16 +16,7 @@ export const Window = React.createClass({
       {...this.getActions()}
       style={{
         ...this.defaultStyle,
-        bottom: 0,
-        left: 0,
-        overflow: 'scroll',
-        position: 'absolute',
-        right: 0,
-        top: 0,
-        WebkitOverflowScrolling: 'touch',
-        WebkitTextSizeAdjust: '100%',
-        MozTextSizeAdjust: '100%',
-        MsTextSizeAdjust: '100%',
+        height: '100%',
         background,
         color,
         fontFamily,
