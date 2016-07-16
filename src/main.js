387a0458d998e710e9c13471bfb8745f118ad0a5
Luis Carli <luiscarli@kensho.com>
2016-07-16 15:51:09 -0400

    main: remove unused

--- src/main.js
+++ src/main.js
@@ -7,23 +7,28 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom'
-// import Relay from 'react-relay'
 import _ from 'lodash/fp'
 import {Router, Route, browserHistory} from 'react-router'
 
+import {pagesData} from './site/pagesData'
 import {App} from './App'
+import defaultTheme from './defaultTheme'
 
 _.map = _.map.convert({cap: false})
 
 document.title = 'Project'
-document.body.bgColor = 'black'
-
-// Relay.injectNetworkLayer(
-//   new Relay.DefaultNetworkLayer('http://localhost:5000/graphql')
-// )
+document.body.bgColor = defaultTheme.base.background
 
 ReactDOM.render(
   <Router history={browserHistory}>
+    <Route path='page'>
+    {_.map((d, i) =>
+      <Route path={`${_.first(d.path.split('.'))}`}
+        component={d.default} key={i}
+      />,
+      pagesData,
+    )}
+    </Route>
     <Route path='*' component={App}/>
   </Router>
   , document.getElementById('root')
