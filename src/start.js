22af0a42707cffd86cadbdb0f746893b28d7baba
Luis Carli <luiscarli@kensho.com>
2016-09-05 10:16:57 -0400

    (FTR) accept functions on extensions

--- src/start.js
+++ src/start.js
@@ -22,12 +22,30 @@ export const start = (
   themeExtensions = [], cssObjectExtensions = [], uiExtensions = []
 ) => {
   merge(theme, vanillaTheme)
-  themeExtensions.forEach(d => merge(theme, d))
+  themeExtensions.forEach(d => {
+    if (_.isFunction(d)) {
+      merge(theme, d(theme))
+      return
+    }
+    merge(theme, d)
+  })
 
   const cssObject = createCssObject(theme)
-  cssObjectExtensions.forEach(d => merge(cssObject, d))
+  cssObjectExtensions.forEach(d => {
+    if (_.isFunction(d)) {
+      merge(cssObject, d(theme, cssObject))
+      return
+    }
+    merge(cssObject, d)
+  })
 
   merge(classNames, getClassesFromCssObject(cssObject))
 
-  uiExtensions.forEach(d => merge(ui, d))
+  uiExtensions.forEach(d => {
+    if (_.isFunction(d)) {
+      merge(ui, d(theme, cssObject, ui))
+      return
+    }
+    merge(ui, d)
+  })
 }
